4e40ba1f038b316d31c3a50b012d85d73756395b
Viktor Salimonov <viktor.salimonov@outlook.com>
2019-05-12 21:33:10 +0300

    basic project structure

---
+++ config/settings/local.py
@@ -0,0 +1,20 @@
+import os
+
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INSTALLED_APPS += [
+    'django_extensions',
+]

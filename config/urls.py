ce6ab95b9d8d1e1bf9fe43c9f374ba499201e80e
Viktor Salimonov <viktor.salimonov@outlook.com>
2019-05-13 01:48:47 +0300

    add namespaces

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('langscape.articles.urls')),
+    url(r'', include('langscape.articles.urls', namespace='articles')),
     url(r'^admin/', admin.site.urls),
 ]

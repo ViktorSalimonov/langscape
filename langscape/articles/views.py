77084f2a52cab8cbdc07d6613dc80a661ee94871
Viktor Salimonov <viktor.salimonov@outlook.com>
2019-05-17 01:37:44 +0300

    add article action mixin

--- langscape/articles/views.py
+++ langscape/articles/views.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic import CreateView, UpdateView, DeleteView
 
 from .models import Article
 from .utils import check_member_rights
+
+
+class ArticleActionMixin(object):
+    fields = ['title', 'difficulty', 'text']
+
+    @property
+    def success_msg(self):
+        return NotImplemented
+
+    def form_valid(self, form):
+        messages.info(self.request, self.success_msg)
+        return super(ArticleActionMixin, self).form_valid(form)
 
 
 class ArticleListView(ListView):
@@ -18,24 +31,16 @@ class ArticleDetailView(DetailView):
     model = Article
 
 
-class ArticleCreateView(LoginRequiredMixin, CreateView):
+class ArticleCreateView(LoginRequiredMixin, ArticleActionMixin, CreateView):
     model = Article
-    fields = ['title', 'difficulty', 'text']
-
-    def form_valid(self, form):
-        return super(ArticleCreateView, self).form_valid(form)
-
-    def get_success_url(self):
-        return reverse('articles:list')
+    success_msg = "Aricle is created!"
+    success_url = reverse_lazy('articles:list')
 
 
-class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, ArticleActionMixin, UpdateView):
     model = Article
-    fields = ['title', 'difficulty', 'text']
-    template_name_suffix = '_update_form'
-
-    def get_success_url(self):
-        return reverse('articles:detail', kwargs={'pk': self.object.pk})
+    success_msg = "Aricle is updated!"
+    success_url = reverse_lazy('articles:list')
 
 
 class ArticleDeleteView(DeleteView):

27d530a6af42b424a63e52e788509b0df473b455
Viktor Salimonov <viktor.salimonov@outlook.com>
2019-09-03 22:53:38 +0300

    create simple article model

---
+++ langscape/articles/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from langscape.articles.models import Article
+
+
+admin.site.register(Article)
--- langscape/articles/models.py
+++ langscape/articles/models.py
@@ -1,10 +1,26 @@
+import uuid as uuid_lib
+
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class TimeStampedModel(models.Model):
+    """Extend standard model."""
+
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
+
+
+class Article(TimeStampedModel):
+    """Article model."""
+
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title

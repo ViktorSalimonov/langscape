1725aa1e14c89102da9940a87c025c0daa8a26af
Viktor Salimonov <viktor.salimonov@outlook.com>
2019-09-06 23:14:42 +0300

    add article detail view and template

--- langscape/articles/urls.py
+++ langscape/articles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from langscape.articles.views import ArticleListView
+from langscape.articles.views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    url(r'^$', ArticleListView.as_view(), name='article-list'),
+    url(r'^$', ArticleListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', ArticleDetailView.as_view(), name='details'),
 ]
--- langscape/articles/views.py
+++ langscape/articles/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from langscape.articles.models import Article
 
@@ -10,3 +11,11 @@ class ArticleListView(ListView):
     template_name = "article_list.html"
     context_object_name = "articles"
     paginate_by = 10
+
+
+class ArticleDetailView(DetailView):
+    """Common detail view for an article."""
+
+    model = Article
+    template_name = "article_detail.html"
+    context_object_name = "article"
